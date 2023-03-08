ac2c98c83d6654eef72ea9d662a8ef6460fe5ff4
Vitor Markis <vitormarkis2369@gmail.com>
2023-03-08 20:52:11 -0300

    create post likes request body schema on server

--- server/src/schemas/post_likes.ts
+++ server/src/schemas/post_likes.ts
@@ -2,6 +2,10 @@ import { z } from "zod"
 import { postSchema } from "./posts"
 import { userSchema } from "./users"
 
+/**
+ * GENERAL
+ */
+
 export const postLikesSchema = z.object({
   post_like_id: z.number().positive(),
   user_id: userSchema.shape.id,
@@ -9,3 +13,12 @@ export const postLikesSchema = z.object({
 })
 
 export type IPostLikes = z.infer<typeof postLikesSchema>
+
+/**
+ * BODY
+ */
+export const postLikesBodySchema = postLikesSchema.pick({
+  post_id: true
+})
+
+export type IPostLikesBody = z.infer<typeof postLikesBodySchema>
